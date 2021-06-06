dffcbe7fce50d9386b7a3b4b699898babcb4ec8d
hajeongcheol <ha.jeongcheol@gmail.com>
2021-06-06 16:37:10 +0900

    first

---
+++ src/main/java/lecture/Course.java
@@ -0,0 +1,88 @@
+package lecture;
+
+import javax.persistence.*;
+import org.springframework.beans.BeanUtils;
+
+@Entity
+@Table(name = "Course_table")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String teacher;
+    private Long fee;
+    private String textBook;
+    private Boolean openYn;
+
+    @PostPersist
+    public void onPostPersist() {
+        CourseRegistered courseRegistered = new CourseRegistered();
+        BeanUtils.copyProperties(this, courseRegistered);
+        courseRegistered.publishAfterCommit();
+    }
+
+    @PostUpdate
+    public void onPostUpdate() {
+        CourseModified courseModified = new CourseModified();
+        BeanUtils.copyProperties(this, courseModified);
+        courseModified.publishAfterCommit();
+    }
+
+    @PreRemove
+    public void onPreRemove() {
+        CourseDeleted courseDeleted = new CourseDeleted();
+        BeanUtils.copyProperties(this, courseDeleted);
+        courseDeleted.publishAfterCommit();
+    }
+
+    public Boolean getOpenYn() {
+        return openYn;
+    }
+
+    public void setOpenYn(Boolean openYn) {
+        this.openYn = openYn;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public String getTextBook() {
+        return textBook;
+    }
+
+    public void setTextBook(String textBook) {
+        this.textBook = textBook;
+    }
+
+    public Long getFee() {
+        return fee;
+    }
+
+    public void setFee(Long fee) {
+        this.fee = fee;
+    }
+
+}
